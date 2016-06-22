1e15bdba5c20aafb3afae3538f6940bdd1293f3c
Dante Cunurana <dangenius.14@hotmail.com>
2016-06-21 20:59:24 -0500

    segundo commit

--- django_apps/cobranzas/urls.py
+++ django_apps/cobranzas/urls.py
@@ -7,7 +7,7 @@ app_name = 'cobranzas'
 urlpatterns = [
 
     url('(?P<empleado_slug>[\w-]+)/(?P<cliente_slug>[\w-]+)/nuevo-cobro/$', views.CrearCobro, name='nuevo_cobro'),
-    url('(?P<empleado_slug>[\w-]+)/(?P<cliente_slug>[\w-]+)/(?P<cobro_slug>[\w-]+)/eliminar/$', views.EliminarCobro.as_view(), name='eliminar_cobro'),
+   url('(?P<empleado_slug>[\w-]+)/(?P<cliente_slug>[\w-]+)/(?P<cobro_slug>[\w-]+)/eliminar/$', views.EliminarCobro.as_view(), name='eliminar_cobro')
     # EDITAR COBRO
     url('(?P<empleado_slug>[\w-]+)/(?P<cliente_slug>[\w-]+)/(?P<cobro_slug>[\w-]+)/editar/$', views.EditarCobro, name='editar_cobro'),
     # DETALLE COBRO
