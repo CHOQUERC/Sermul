0b6ecff506026b48daf2681ce1f323764e26affc
Dante Cunurana <dangenius.14@hotmail.com>
2016-06-21 21:10:00 -0500

    tercer commit

--- django_apps/cobranzas/urls.py
+++ django_apps/cobranzas/urls.py
@@ -6,16 +6,16 @@ app_name = 'cobranzas'
 
 urlpatterns = [
 
-    url('(?P<empleado_slug>[\w-]+)/(?P<cliente_slug>[\w-]+)/nuevo-cobro/$', views.CrearCobro, name='nuevo_cobro'),
-   url('(?P<empleado_slug>[\w-]+)/(?P<cliente_slug>[\w-]+)/(?P<cobro_slug>[\w-]+)/eliminar/$', views.EliminarCobro.as_view(), name='eliminar_cobro')
+    url(r'^(?P<empleado_slug>[\w-]+)/(?P<cliente_slug>[\w-]+)/nuevo-cobro/$', views.CrearCobro, name='nuevo_cobro'),
+    url(r'^(?P<empleado_slug>[\w-]+)/(?P<cliente_slug>[\w-]+)/(?P<cobro_slug>[\w-]+)/eliminar/$', views.EliminarCobro.as_view(), name='eliminar_cobro'),
     # EDITAR COBRO
-    url('(?P<empleado_slug>[\w-]+)/(?P<cliente_slug>[\w-]+)/(?P<cobro_slug>[\w-]+)/editar/$', views.EditarCobro, name='editar_cobro'),
+    url(r'^(?P<empleado_slug>[\w-]+)/(?P<cliente_slug>[\w-]+)/(?P<cobro_slug>[\w-]+)/editar/$', views.EditarCobro, name='editar_cobro'),
     # DETALLE COBRO
-    url('(?P<empleado_slug>[\w-]+)/(?P<cliente_slug>[\w-]+)/(?P<cobro_slug>[\w-]+)/$', views.DetalleCobro.as_view(), name='detalle_cobro'),
+    url(r'^(?P<empleado_slug>[\w-]+)/(?P<cliente_slug>[\w-]+)/(?P<cobro_slug>[\w-]+)/$', views.DetalleCobro.as_view(), name='detalle_cobro'),
     # NUEVO COBRO
     # ELMINAR COBRO
     # LISTAR COBRO
-    url('(?P<empleado_slug>[\w-]+)/(?P<cliente_slug>[\w-]+)/$', views.ListaCobros.as_view(), name='listar_cobros'),
+    url(r'^(?P<empleado_slug>[\w-]+)/(?P<cliente_slug>[\w-]+)/$', views.ListaCobros.as_view(), name='listar_cobros'),
 
 
 ]
